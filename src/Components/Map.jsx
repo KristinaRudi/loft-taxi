cfa050d78140409b821082bee231ab6641fca7e5
Kristina <kristina-rudi@mail.ru>
2021-08-17 19:01:09 +0300

    17/08/2021

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
+import mapbox from 'mapbox';
+import gl from 'gl';
 import '../../src/Styles/map.css';
 
 
-export const Map = ({children}) => {
-return (<div className="map">
-    { children }
-</div>)
+export class Map extends Component {
+    mapContainer = React.createRef();
+    render() {
+        return <div className="map-wrapper">
+            <div data-testid="map" className="map" ref={this.mapContainer} />
+            {this.props.children}
+        </div>;
+    }
 }
+
+
+// export const Map = ({children}) => {
+// return ()
+// }
 
 export default Map;
